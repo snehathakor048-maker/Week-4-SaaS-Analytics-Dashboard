bad89345db41a8e47131fc512782afd708673d3a
snehathakor048-maker <snehathakor048@gmail.com>
2026-03-17 17:54:30 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
+import { mockDashboardData } from "./data.js"
 
+const table = document.getElementById("tableData")
+
+function renderTable(){
+
+table.innerHTML=""
+
+mockDashboardData.forEach(user=>{
+
+const row = document.createElement("tr")
+
+row.innerHTML=`
+
+<td>${user.name}</td>
+<td>${user.email}</td>
+<td>$${user.revenue}</td>
+<td class="${user.status.toLowerCase()}">${user.status}</td>
+<td>${user.lastLogin}</td>
+
+`
+
+table.appendChild(row)
+
+})
+
+}
+
+renderTable()
